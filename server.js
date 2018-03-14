c3c06b5a3561384e945fd4cbc4c8cedc6786dc58
Gokul Nair <gokulgnair94@gmail.com>
2018-03-14 22:28:09 +0530

    first

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const restService = express();
+var https = require('https');
+var fs = require('fs'),
+    path = require('path');
+restService.use(bodyParser.urlencoded({
+    extended: true
+}));
+restService.use(bodyParser.json());
+
+
+restService.post('/',function( req,res ){
+    console.log( "Email : " + req.body.email );
+});
+
+restService.listen((process.env.PORT || 9000), function() {
+  console.log("Server up and listening");
+});
